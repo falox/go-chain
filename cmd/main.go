57d2c5e9f230170b977e744c1f028643fd98ac72
Alberto Falossi <26248823+falox@users.noreply.github.com>
2020-08-16 00:35:24 +0200

    Implemented blockchain

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,141 @@
 package main
 
-import "fmt"
+import (
+	"crypto/sha256"
+	"encoding/hex"
+	"errors"
+	"fmt"
+	"strconv"
+	"strings"
+	"time"
+)
+
+const genesisBlockData = "The Times 03/Jan/2009 Chancellor on brink of second bailout for banks" // https://en.bitcoin.it/wiki/Genesis_block
+
+// Block is the element of the Blockchain.
+type Block struct {
+	Timestamp    string
+	Data         string
+	PreviousHash string
+	Hash         string
+	Nonce        int
+}
+
+var difficulty int = 1
+
+func calculateHash(block Block) string {
+	data := block.Timestamp + block.Data + block.PreviousHash + strconv.Itoa(block.Nonce)
+	hash := sha256.Sum256([]byte(data))
+	return hex.EncodeToString(hash[:])
+}
+
+func getLatestBlock(blockchain []Block) Block {
+	// ASSUMPTION: Blockchain has at least 1 (genesis) block
+	return blockchain[len(blockchain)-1]
+}
+
+func isBlockchainValid(blockchain []Block) bool {
+	// Empty blockchain is not valid
+	if len(blockchain) == 0 {
+		return false
+	}
+
+	// Validate the genesis block
+	if len(blockchain) > 0 && blockchain[0].Data != genesisBlockData {
+		return false
+	}
+
+	// Validate the subsequent blocks
+	for i := 1; i < len(blockchain); i++ {
+		currentBlock := blockchain[i]
+		previousBlock := blockchain[i-1]
+
+		if currentBlock.Hash != calculateHash(currentBlock) {
+			return false
+		}
+
+		if currentBlock.PreviousHash != previousBlock.Hash {
+			return false
+		}
+	}
+
+	return true
+}
+
+func createBlockchain(timestamp time.Time) []Block {
+	genesisBlock := Block{}
+	genesisBlock.Timestamp = timestamp.Format(time.RFC3339)
+	genesisBlock.PreviousHash = strings.Repeat("0", 64)
+	genesisBlock.Data = genesisBlockData
+	genesisBlock.Nonce = 0
+
+	blockchain := make([]Block, 0, 10)
+
+	addBlock(&blockchain, genesisBlock)
+
+	return blockchain
+}
+
+func createBlock(timestamp time.Time, data string, previousBlock Block) (block Block, err error) {
+	if len(data) == 0 {
+		err = errors.New("Cannot create a block with no data")
+		return
+	}
+
+	block = Block{}
+	block.Timestamp = timestamp.Format(time.RFC3339)
+	block.PreviousHash = previousBlock.Hash
+	block.Data = data
+	block.Nonce = 0
+	return
+}
+
+func mineBlock(block Block) Block {
+	targetHashPrefix := strings.Repeat("0", difficulty)
+
+	for {
+		block.Hash = calculateHash(block)
+
+		if strings.HasPrefix(block.Hash, targetHashPrefix) {
+			return block
+		}
+
+		block.Nonce++
+	}
+}
+
+func addBlock(blockchain *[]Block, block Block) (err error) {
+	minedBlock := mineBlock(block)
+
+	candidateBlockchain := append(*blockchain, minedBlock)
+	if isBlockchainValid(candidateBlockchain) == false {
+		return errors.New("Invalid block (Previous hash is inconsistent with the chain). Blockchain was not updated")
+	}
+
+	*blockchain = candidateBlockchain
+	return
+}
+
+func dumpBlockchain(blockchain []Block) {
+	for i, block := range blockchain {
+		fmt.Printf("Block %d: \"%s\" (%s)\n", i+1, block.Data, block.Hash)
+	}
+
+	if isBlockchainValid(blockchain) {
+		fmt.Printf("Blockchain is VALID\n")
+	} else {
+		fmt.Printf("Blockchain is NOT VALID\n")
+	}
+}
 
 func main() {
-	fmt.Println("Hello, World")
+	blockchain := createBlockchain(time.Now())
+
+	block, _ := createBlock(time.Now(), "first block", getLatestBlock(blockchain))
+	addBlock(&blockchain, block)
+
+	block, _ = createBlock(time.Now(), "second block", getLatestBlock(blockchain))
+	addBlock(&blockchain, block)
+
+	dumpBlockchain(blockchain)
 }
---
+++ cmd/main_test.go
@@ -0,0 +1,164 @@
+package main
+
+import (
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCreateBlockchain(t *testing.T) {
+	// Arrange
+	now, _ := time.Parse(time.RFC3339, "2020-08-10T00:00:00.000Z")
+
+	// Act
+	result := createBlockchain(now)
+
+	// Assert
+	expectedGenesisBlock := Block{now.Format(time.RFC3339), genesisBlockData, strings.Repeat("0", 64), "0f723d11ee6c1bb2609ce20084c1c4e062e015a5d039e3ad7cf2c7eac5d3024c", 5}
+	assert.Equal(t, expectedGenesisBlock, result[0])
+}
+
+func TestCreateBlock(t *testing.T) {
+	// Arrange
+	now := time.Now()
+
+	// Act
+	result, _ := createBlock(now, "Data", Block{Hash: "PreviousHash"})
+
+	// Assert
+	expected := Block{now.Format(time.RFC3339), "Data", "PreviousHash", "", 0}
+	assert.Equal(t, expected, result)
+}
+
+func TestCreateBlockWithNoData(t *testing.T) {
+	// Act
+	_, err := createBlock(time.Now(), "", Block{})
+
+	// Assert
+	assert.NotEqual(t, nil, err)
+}
+
+func TestAddBlock(t *testing.T) {
+	// Arrange
+	blockchain := createBlockchain(time.Now())
+	block, _ := createBlock(time.Now(), "block", getLatestBlock(blockchain))
+
+	// Act
+	err := addBlock(&blockchain, block)
+
+	// Assert
+	assert.Equal(t, nil, err)
+	assert.NotEqual(t, "", getLatestBlock(blockchain).Hash)
+	assert.Equal(t, 2, len(blockchain))
+}
+
+func TestAddBlockWithInvalidBLock(t *testing.T) {
+	// Arrange
+	blockchain := createBlockchain(time.Now())
+	block, _ := createBlock(time.Now(), "detached block", Block{})
+
+	// Act
+	err := addBlock(&blockchain, block)
+
+	// Assert
+	assert.NotEqual(t, nil, err)
+	assert.Equal(t, 1, len(blockchain))
+}
+
+func TestCalculateHash(t *testing.T) {
+	// Arrange
+	block := Block{Data: "Data", Timestamp: "Timestamp", PreviousHash: "PreviousHash", Nonce: 123}
+
+	// Act
+	result := calculateHash(block)
+
+	// Assert
+	const EXPECTED = "ecfd4574e62af7d96875cf1a96b59f784aed4937387f5a62cf8691739401ec37"
+	assert.Equal(t, EXPECTED, result)
+}
+
+func TestCalculateHashWithSameInput(t *testing.T) {
+	// Arrange
+	block1 := Block{Data: "Data", Timestamp: "Timestamp", PreviousHash: "PreviousHash", Nonce: 123}
+	block2 := Block{Data: "Data", Timestamp: "Timestamp", PreviousHash: "PreviousHash", Nonce: 123}
+
+	// Act
+	result1 := calculateHash(block1)
+	result2 := calculateHash(block2)
+
+	// Assert
+	assert.Equal(t, result1, result2)
+}
+
+func TestCalculateHashWithDifferentInput(t *testing.T) {
+	// Arrange
+	block := Block{Data: "Data", Timestamp: "Timestamp", PreviousHash: "PreviousHash", Nonce: 123}
+	result1 := calculateHash(block)
+
+	// Act
+	block.Nonce = 124
+	result2 := calculateHash(block)
+
+	// Assert
+	assert.NotEqual(t, result1, result2)
+}
+
+func TestIsBlockchainValid(t *testing.T) {
+	// Arrange
+	blockchain := createBlockchain(time.Now())
+	block, _ := createBlock(time.Now(), "block", getLatestBlock(blockchain))
+	addBlock(&blockchain, block)
+
+	// Act
+	result := isBlockchainValid(blockchain)
+
+	// Assert
+	assert.Equal(t, true, result)
+}
+
+func TestIsBlockchainValidWithGenesisBlockOnly(t *testing.T) {
+	// Arrange
+	blockchain := createBlockchain(time.Now())
+
+	// Act
+	result := isBlockchainValid(blockchain)
+
+	// Assert
+	assert.Equal(t, true, result)
+}
+
+func TestIsBlockchainValidWithTamperedGenesisBlock(t *testing.T) {
+	// Arrange
+	blockchain := createBlockchain(time.Now())
+	blockchain[0].Data = "TAMPERED"
+
+	// Act
+	result := isBlockchainValid(blockchain)
+
+	// Assert
+	assert.Equal(t, false, result)
+}
+
+func TestIsBlockchainValidWithTamperedBlock(t *testing.T) {
+	// Arrange
+	blockchain := createBlockchain(time.Now())
+	block, _ := createBlock(time.Now(), "block", getLatestBlock(blockchain))
+	addBlock(&blockchain, block)
+	blockchain[len(blockchain)-1].Data = "TAMPERED"
+
+	// Act
+	result := isBlockchainValid(blockchain)
+
+	// Assert
+	assert.Equal(t, false, result)
+}
+
+func TestIsBlockchainValidWithEmptyBlock(t *testing.T) {
+	// Act
+	result := isBlockchainValid([]Block{})
+
+	// Assert
+	assert.Equal(t, false, result)
+}
