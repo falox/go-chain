31a782383847e7b38181ed3bfd31cf6e46a501f5
Alberto Falossi <26248823+falox@users.noreply.github.com>
2020-08-18 16:38:32 +0200

    Changed dumpBlockchain to use a pointer to the blockchain

--- cmd/main.go
+++ cmd/main.go
@@ -5,12 +5,12 @@ import (
 	"time"
 )
 
-func dumpBlockchain(blockchain []Block) {
-	for i, block := range blockchain {
+func dumpBlockchain(blockchain *[]Block) {
+	for i, block := range *blockchain {
 		fmt.Printf("Block %d: \"%s\" (%s)\n", i+1, block.Data, block.Hash)
 	}
 
-	if isBlockchainValid(blockchain) {
+	if isBlockchainValid(*blockchain) {
 		fmt.Printf("Blockchain is VALID\n")
 	} else {
 		fmt.Printf("Blockchain is NOT VALID\n")
@@ -26,5 +26,5 @@ func main() {
 	block, _ = createBlock(time.Now(), "second block", getLatestBlock(blockchain))
 	addBlock(&blockchain, block)
 
-	dumpBlockchain(blockchain)
+	dumpBlockchain(&blockchain)
 }
